e5fd3635230fbfb9d16ee6e3a8dbf3900186b860
it21004490 <it21004490@my.sliit.lk>
2023-11-02 01:09:49 +0530

    Initiate commit

---
+++ twitter_etl.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import tweepy
+import json
+from datetime import datetime
+import s3fs
+
+
+access_key = "80kB7Wqqj00iqBuqPqfoX72fQ"
+access_secret = "O13fanvK0vjyOuloXcitnxw5yUi3ATwcOuWiKdRCbOyuO5cSdo"
+consumer_key = "1441419697149255680-1MQNrJJhWCP4sqA2rTFvHnwnsHpcy3"
+consumer_secret = "ozoOKbgcH1keqibNpphyOb6IqmSjdDIAQqN6hLkYUuEJS"
